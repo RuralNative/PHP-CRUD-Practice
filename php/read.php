3e6af89e036fd67b094f24238d56fc2748a41fce
John Berlin Leonor <117552434+RuralNative@users.noreply.github.com>
2023-06-20 10:26:03 +0800

    Added READ PHP for Database Table

---
+++ php/read.php
@@ -0,0 +1,33 @@
+<?php
+  require_once 'config.php';
+  
+  $sql = "SELECT * FROM students";
+
+   // Execute the SQL statement
+   $result = mysqli_query($conn, $sql);
+
+   // Check if the query was successful
+   if ($result) {
+       // Loop through the result set
+       while ($row = mysqli_fetch_assoc($result)) {
+           // Access the retrieved data
+           $first_name = $row['first_name'];
+           $last_name = $row['last_name'];
+           $course = $row['course'];
+           $year_level = $row['year_level'];
+           $class_section = $row['class_section'];
+
+           // Display the data in the table row
+           echo "<tr>";
+           echo "<td><span class='custom-checkbox'><input type='checkbox' id='checkbox1' name='options[]' value='1'><label for='checkbox1'></label></span></td>";
+           echo "<td>$first_name</td>";
+           echo "<td>$last_name</td>";
+           echo "<td>$course</td>";
+           echo "<td>$year_level</td>";
+           echo "<td>$class_section</td>";
+           echo "<td><a href='#editEmployeeModal' class='edit' data-toggle='modal'><i class='material-icons' data-toggle='tooltip' title='Edit'>&#xE254;
+           </i></a><a href='#deleteEmployeeModal' class='delete' data-toggle='modal'><i class='material-icons' data-toggle='tooltip' title='Delete'>&#xE872;</i></a></td>";
+           echo "</tr>";
+       }
+   }
+?>
